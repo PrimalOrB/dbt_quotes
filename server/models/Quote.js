0fc67ace6843b6de5d963bbeffaa2a6082c822f8
Andrew Ogilvie <primalorb@gmail.com>
2021-07-09 13:30:17 -0400

    initial react and graphql implementation

---
+++ server/models/Quote.js
@@ -0,0 +1,63 @@
+const { Schema, model } = require('mongoose');
+const noteSchema = require('./Note');
+const dateFormat = require('../utils/dateFormat');
+
+const quoteSchema = new Schema(
+  {
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: timestamp => dateFormat(timestamp)
+    },
+    customerName: {
+        type: String,
+        required: 'Customer name required',
+    },
+    jNum: {
+        type: Number,
+        required: false
+    },
+    description: {
+        type: String,
+        required: 'Description required',
+    },
+    priority: {
+        type: Number,
+        min: 1,
+        max: 5
+    },
+    additionalNotes: {
+        type: String,
+    },
+    pcsURL: {
+        type: String,
+    },
+    crmURL: {
+        type: String,
+    },
+    status: {
+        type: String,
+    },
+    PODate: {
+        type: Date,
+        get: timestamp => dateFormat(timestamp)
+    },
+    POQty: {
+        type: Number,
+    },
+    notes: [noteSchema]
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+quoteSchema.virtual('noteCount').get(function() {
+  return this.notes.length;
+});
+
+const Quote = model('Quote', quoteSchema);
+
+module.exports = Quote;
---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Thought = require('./Thought');
+const Quote = require('./Quote');
+
+module.exports = { User, Thought, Quote };
