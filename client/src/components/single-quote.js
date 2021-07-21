7a927d50cf5188a437b3ea1cab22dc106fad451e
Andrew Ogilvie <primalorb@gmail.com>
2021-07-21 06:20:11 -0400

    add some helper functions

--- client/src/components/single-quote.js
+++ client/src/components/single-quote.js
@@ -7,68 +7,42 @@ import { FiEdit, FiExternalLink } from 'react-icons/fi'
 import { HiOutlineExclamation } from 'react-icons/hi'
 import { MdForum } from 'react-icons/md'
 import { CardLink } from './'
+import { format_plural, format_label_case, calc_days_between, calc_days_to_current } from "../utils/helpers";
 
 const SingleQuote = ( { quoteData } ) => {
-
-    function dateDiff( a ){
-        const b = new Date()
-        a = new Date(Number(a))
-        const day = 1000 * 60 * 60 * 24;
-        const timeDiff = b.getTime() - a.getTime();
-        const diff = Math.round(timeDiff / day);
-        return { diff };
-    }
-
-    function transformLabel( label ) {
-        if( label.length === 3 ) {
-            return label.toUpperCase()
-        } else {
-            function capitalize(str) {
-                return str.charAt(0).toUpperCase() + str.slice(1);
-            }
-            return label.split('-').map( capitalize ).join(' ');
-            }
-    }
-
-    function daysBetween( a, b){
-        var delta = Math.abs(a - b) / 1000;
-        var days = Math.floor(delta / 86400);
-        const str =  Number(days) === 1 ? ' day' : ' days'
-        return days + str
-    }
-        
+      
     return (
         <>
-        <article className="quoteCard" key={ quoteData._id }>
-                    <div className="quoteCardTop">
-                        {quoteData.priority > 0 && (
-                            <RotatedEl text={ quoteData.priority } hover="Priority:" data={ quoteData.priority } colorRange='5' icon={ HiOutlineExclamation() }/>
-                            )
-                        } 
-                        <RotatedEl text={ dateDiff( quoteData.createdAt ).diff } hover="Task Date:" data={ quoteData.createdAt } colorRange='30'  icon={ BsCalendar() }/>
-                        {quoteData.PODate > 0 && (
-                            <RotatedEl text={ dateDiff( quoteData.PODate, 30 ).diff } hover="PO Qty:" data={ quoteData.POQty  } colorRange='30' icon={ FaDollarSign() }/>
-                            )
-                        } 
-                        <div className="details">
-                            <div className="sub-details">
-                                { quoteData.jNum && <div className='jnum'>{ `J${ quoteData.jNum } :` }</div> }
-                                <div className='customer'>{ quoteData.customerName }</div>
-                                <div className='desc'>- { quoteData.description }</div>
-                            </div>
-                            <div className={ `status ${ quoteData.status }` }>{ transformLabel( quoteData.status ) }</div>
-                            <div className="additional">{ quoteData.additionalNotes }</div>
+            <article className="quoteCard" key={ quoteData._id }>
+                <div className="quoteCardTop">
+                    {quoteData.priority > 0 && (
+                        <RotatedEl text={ quoteData.priority } hover="Priority:" data={ quoteData.priority } colorRange='5' icon={ HiOutlineExclamation() }/>
+                        )
+                    } 
+                    <RotatedEl text={ calc_days_to_current( quoteData.createdAt ).diff } hover="Task Date:" data={ quoteData.createdAt } colorRange='30'  icon={ BsCalendar() }/>
+                    {quoteData.PODate > 0 && (
+                        <RotatedEl text={ calc_days_to_current( quoteData.PODate, 30 ).diff } hover="PO Qty:" data={ quoteData.POQty  } colorRange='30' icon={ FaDollarSign() }/>
+                        )
+                    } 
+                    <div className="details">
+                        <div className="sub-details">
+                            { quoteData.jNum && <div className='jnum'>{ `J${ quoteData.jNum } :` }</div> }
+                            <div className='customer'>{ quoteData.customerName }</div>
+                            <div className='desc'>- { quoteData.description }</div>
                         </div>
+                        <div className={ `status ${ quoteData.status }` }>{ format_label_case( quoteData.status ) }</div>
+                        <div className="additional">{ quoteData.additionalNotes }</div>
                     </div>
-                    <div className="quoteCardBottom">
-                        <Link className="cardLink" to={`/quote/${quoteData._id}`} ><p><MdForum/>Conversation { quoteData.noteCount > 0 && <span className='note-count' >( { quoteData.noteCount } note{ quoteData.noteCount > 1 && 's' } )</span>}</p></Link>
-                        { quoteData.completedDate > 0 && ( <span>Task Duration: { daysBetween( quoteData.completedDate, quoteData.createdAt )  }</span>) }
-                        <div className="quoteLinks">
-                            { ( quoteData.crmURL !== '' && quoteData.crmURL !== null)  && <CardLink icon={ FiExternalLink() } text={ "crm" } url={ quoteData.crmURL } target={ "_blank "} />}
-                            { ( quoteData.pcsURL !== '' && quoteData.pcsURL !== null)  && <CardLink icon={ FiExternalLink() } text={ "pcs" } url={ quoteData.pcsURL } target={ "_blank "}/>}
-                            <Link className="cardLink" to={`/edit/${quoteData._id}`} ><p><FiEdit/>edit</p></Link>
-                        </div>
+                </div>
+                <div className="quoteCardBottom">
+                    <Link className="cardLink" to={`/quote/${quoteData._id}`} ><p><MdForum/>Conversation { quoteData.noteCount > 0 && <span className='note-count' >( { quoteData.noteCount } { format_plural( 'note', quoteData.noteCount ) } )</span>}</p></Link>
+                    { quoteData.completedDate > 0 && ( <span>Task Duration: { calc_days_between( quoteData.completedDate, quoteData.createdAt ) } { format_plural( 'day', calc_days_between( quoteData.completedDate, quoteData.createdAt ) )  }</span>) }
+                    <div className="quoteLinks">
+                        { ( quoteData.crmURL !== '' && quoteData.crmURL !== null)  && <CardLink icon={ FiExternalLink() } text={ "crm" } url={ quoteData.crmURL } target={ "_blank "} />}
+                        { ( quoteData.pcsURL !== '' && quoteData.pcsURL !== null)  && <CardLink icon={ FiExternalLink() } text={ "pcs" } url={ quoteData.pcsURL } target={ "_blank "}/>}
+                        <Link className="cardLink" to={`/edit/${quoteData._id}`} ><p><FiEdit/>edit</p></Link>
                     </div>
+                </div>
             </article>
         </>
     );
---
+++ client/src/utils/helpers.js
@@ -0,0 +1,31 @@
+module.exports = {
+    format_plural: ( word, amount ) => {
+        if( amount !== 1 ) {
+            return `${word}s`
+        }
+        return word
+    },
+    format_label_case: ( label ) => {
+        if( label.length === 3 ) {
+            return label.toUpperCase()
+        } else {
+            function capitalize(str) {
+                return str.charAt(0).toUpperCase() + str.slice(1);
+            }
+            return label.split('-').map( capitalize ).join(' ');
+        }
+    },
+    calc_days_between: ( dateA, dateB ) => {
+        var delta = Math.abs( dateA - dateB) / 1000;
+        var days = Math.floor( delta / 86400 );
+        return days
+    },
+    calc_days_to_current: ( date ) => {
+        const b = new Date()
+        date = new Date( Number( date ) )
+        const day = 1000 * 60 * 60 * 24;
+        const timeDiff = b.getTime() - date.getTime();
+        const diff = Math.round( timeDiff / day );
+        return { diff };
+    }
+}
